cdedc1408fcadb1d52358541149626a91786eb0d
Valentin Mirontsev <valentin.mironcev@mail.ru>
2024-02-20 19:09:43 +0300

    add task1

--- ConsoleApp1/Program.fs
+++ ConsoleApp1/Program.fs
@@ -1,3 +1,10 @@
-﻿
-printfn "Hello from F#"
+﻿let rec factorial n acc i =
+    if i = n then
+        acc
+    else
+        factorial
+            (n)
+            (acc * (n - i))
+            (i + 1)
 
+//printfn "%d" (factorial 0 1 0)
