c31dfc39f477efee1b42497d93b4e872fadc9341
Valentin Mirontsev <valentin.mironcev@mail.ru>
2024-02-20 19:18:08 +0300

    add fibonacci

--- ConsoleApp1/Program.fs
+++ ConsoleApp1/Program.fs
@@ -6,5 +6,17 @@
             (n)
             (acc * (n - i))
             (i + 1)
+            
+//printfn "%d" (factorial 0 1 0)        
 
-//printfn "%d" (factorial 0 1 0)
+let rec fibonacci n acc1 acc2 i =
+    if i = n then
+        acc2
+    else
+        fibonacci
+            (n)
+            (acc1 + acc2)
+            (acc1)
+            (i + 1)
+
+//printfn "%d" (fibonacci 11 1 0 0)
