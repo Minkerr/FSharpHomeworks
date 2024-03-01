588ab688214629e19cf920966d9e7a5f6ae807e9
Valentin Mirontsev <valentin.mironcev@mail.ru>
2024-03-01 11:21:43 +0300

    fixes

--- Homework1/Homework1/Program.fs
+++ Homework1/Homework1/Program.fs
@@ -1,59 +1,37 @@
-﻿let factorial n = 
-    let rec factorialRecursive n acc i =
-        if i = n then
-            acc
-        else
-            factorialRecursive
-                n
-                (acc * (n - i))
-                (i + 1)   
-    factorialRecursive n 1 0
+﻿let factorial n =
+    if n < 0 then None else
+        let rec factorialRecursive n acc i =
+            if i = n then acc else
+                
+            factorialRecursive n (acc * (n - i)) (i + 1)   
+        Some(factorialRecursive n 1 0)
             
-let fibonacci n = 
-    let rec fibonacciRecursive n acc1 acc2 i =
-        if i = n then
-            acc2
-        else
-            fibonacciRecursive
-                n
-                (acc1 + acc2)
-                acc1
-                (i + 1)
-    fibonacciRecursive n 1 0 0 
+let fibonacci n =
+    if n < 0 then None else
+        let rec fibonacciRecursive n acc1 acc2 i =
+            if i = n then acc2 else
+                
+            fibonacciRecursive n (acc1 + acc2) acc1 (i + 1)
+        Some(fibonacciRecursive n 1 0 0) 
 
-let reverseList list = 
+let reverse list = 
     let rec reverseListRecursive list res =
-        if List.length list = 0 then
-            res
-        else
-            reverseListRecursive
-                (List.tail list)
-                (List.head list :: res)
+        match list with
+         | [] -> res
+         | head :: tail -> reverseListRecursive tail (head :: res)
     reverseListRecursive list []
             
 let degreeRow n m =
     let rec degreeRowRecursive n m acc num i =
-        if n + m + 1 = i then
-            reverseList acc
-        else
-            let acc = if i < n then acc else num :: acc
-            degreeRowRecursive
-                n
-                m
-                acc
-                (num * 2)
-                (i + 1) 
-    degreeRowRecursive n m [] 1 0 
+        if m + 1 = i then acc else
+            
+        degreeRowRecursive n m (num :: acc) (num / 2) (i + 1) 
+    degreeRowRecursive n m [] (int (2.0 ** float (m + n))) 0 
           
-let searchNumberIndex list target = 
-    let rec searchNumberIndexRecursive list target i =
-        if List.length list = 0 then
-            None
-        else if List.head list = target then
-            Some(i)
-        else
-            searchNumberIndexRecursive
-                (List.tail list)
-                target
-                (i + 1)
-    searchNumberIndexRecursive list target 0
+let tryFind list target = 
+    let rec tryFindRecursive list target i =
+        match list with
+         | [] -> None
+         | head :: tail when head = target -> Some(i)
+         | _ -> tryFindRecursive (List.tail list) target (i + 1)
+    tryFindRecursive list target 0
