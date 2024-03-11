e43eb698509a032aa79243c153185f933ba3b94d
Valentin Mirontsev <valentin.mironcev@mail.ru>
2024-03-11 11:45:05 +0300

    small changes

--- Homework1/Homework1/Program.fs
+++ Homework1/Homework1/Program.fs
@@ -2,16 +2,14 @@
     if n < 0 then None else
         let rec factorialRecursive n acc i =
             if i = n then acc else
-                
-            factorialRecursive n (acc * (n - i)) (i + 1)   
+                factorialRecursive n (acc * (n - i)) (i + 1)   
         Some(factorialRecursive n 1 0)
             
 let fibonacci n =
     if n < 0 then None else
         let rec fibonacciRecursive n acc1 acc2 i =
             if i = n then acc2 else
-                
-            fibonacciRecursive n (acc1 + acc2) acc1 (i + 1)
+                fibonacciRecursive n (acc1 + acc2) acc1 (i + 1)
         Some(fibonacciRecursive n 1 0 0) 
 
 let reverse list = 
@@ -24,8 +22,7 @@ let reverse list =
 let degreeRow n m =
     let rec degreeRowRecursive n m acc num i =
         if m + 1 = i then acc else
-            
-        degreeRowRecursive n m (num :: acc) (num / 2) (i + 1) 
+            degreeRowRecursive n m (num :: acc) (num / 2) (i + 1) 
     degreeRowRecursive n m [] (int (2.0 ** float (m + n))) 0 
           
 let tryFind list target = 
