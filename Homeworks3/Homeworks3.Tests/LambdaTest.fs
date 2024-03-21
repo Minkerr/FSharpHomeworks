78853d1695c660e18f0440b60d2a795bdf0e4861
Valentin Mirontsev <valentin.mironcev@mail.ru>
2024-03-22 00:11:55 +0300

    add task 3

---
+++ Homeworks3/Homeworks3.Tests/LambdaTest.fs
@@ -0,0 +1,78 @@
+module Lambda.Tests
+
+open NUnit.Framework
+open Lambda
+open FsUnit
+
+[<Test>]
+let ``FV find all free variables in lambda term`` () =
+    let x = Name("x")
+    let y = Name("y")
+    let z = Name("z")
+    let lambda = App(Abs(x, Abs(y, Var(z))), Abs(x, App(Var(z), Var(x)))) //(λx y.x)(λx.z x)
+    FV lambda |> should contain (Var(z))
+    Set.count (FV lambda) |> should equal 1
+
+[<Test>]
+let ``BV find all bonded variables in lambda term`` () =
+    let x = Name("x")
+    let y = Name("y")
+    let z = Name("z")
+    let lambda = App(Abs(x, Abs(y, Var(z))), Abs(x, App(Var(z), Var(x)))) //(λx y.x)(λx.z x)
+    BV lambda |> should contain (Var(x))
+    BV lambda |> should contain (Var(y))
+    Set.count (BV lambda) |> should equal 2
+    
+[<Test>]
+let ``newVar should find new name for bonded variable`` () =
+    let a = Name("a")
+    let b = Name("b")
+    let c = Name("c")
+    let lambda1 = Abs(c, Abs(c, Var(a))) //(λc c.a)
+    let lambda2 = Abs(c, App(Var(a), Var(b))) //(λc.a b)
+    newVar lambda1 lambda2 |> should equal (Name("c"))
+    
+[<Test>]
+let ``Substraction should work for variables`` () =
+    let x = Name("x")
+    let y = Name("y")
+    let z = Name("z")
+    let term = Abs(z, Var(z))
+    substitution (Var(x)) (Var(x)) term |> should equal term
+    substitution (Var(y)) (Var(x)) term |> should equal (Var(y))
+    
+[<Test>]
+let ``Substitution should work for application`` () =
+    let x = Name("x")
+    let y = Name("y")
+    let z = Name("z")
+    let lambda1 = App(Var(x), Var(x))
+    let lambda2 = Abs(z, Var(z))
+    substitution lambda1 (Var(x)) lambda2 |> should equal (App(lambda2, lambda2))
+    
+[<Test>]
+let ``Substitution should work for abstraction in place of bonded variable`` () =
+    let x = Name("x")
+    let y = Name("y")
+    let z = Name("z")
+    let term = Abs(x, Var(y))
+    substitution term (Var(x)) (Var(z)) |> should equal term
+    
+[<Test>]
+let ``Substitution should work for abstraction without name conflict`` () =
+    let x = Name("x")
+    let y = Name("y")
+    let z = Name("z")
+    let term1 = Abs(y, Var(x))
+    let term2 = Abs(z, Var(z))
+    substitution term1 (Var(x)) term2 |> should equal (Abs(y, Abs(z, Var(z))))
+    
+[<Test>]
+let ``Substitution should work for abstraction with name conflict`` () =
+    let a = Name("a")
+    let x = Name("x")
+    let y = Name("y")
+    let z = Name("z")
+    let term1 = Abs(y, App(Var(x), Var(y)))
+    let term2 = Abs(z, Var(z))
+    substitution term1 (Var(x)) (Var(y)) |> should equal (Abs(a, App(Var(y), Var(a))))
---
+++ Homeworks3/Homeworks3/Lambda.fs
@@ -0,0 +1,45 @@
+module Lambda
+
+type variation = Name of string
+
+type term =
+    |Var of variation
+    |App of term * term
+    |Abs of variation * term
+
+let rec FV lambda =
+    match lambda with
+    | Var(x) -> Set.ofList [Var(x)]
+    | Abs(x, y) -> Set.difference (FV(y)) (Set.ofList [Var(x)])
+    | App(x, y) -> Set.union (FV(x)) (FV(y)) 
+    
+let rec BV lambda =
+    match lambda with
+    | Var(x) -> Set.empty
+    | Abs(x, y) -> Set.union (Set.ofList [Var(x)]) (BV(y)) 
+    | App(x, y) -> Set.union (BV(x)) (BV(y))
+    
+let newVar s t =
+    let rec newVarRec s t var =
+        let varMapped = Var(Name(var))
+        if (not (Set.contains varMapped (FV s))) && (not (Set.contains varMapped (FV t))) then
+            Name(var)
+        else
+            newVarRec s t (string (char(int (char var) + 1)))
+    newVarRec s t "a"
+
+let rec substitution lambda var (t : term) =
+    match lambda with
+    | Var(x) when Var(x) = var ->  t
+    | Var(y) when not (Var(y) = var) -> Var(y)
+    | App(s1, s2) -> App(substitution s1 var t, substitution s2 var t)
+    | Abs(x, s) when Var(x) = var -> Abs(x, s)
+    | Abs(y, s) when (not (Set.contains (Var(y)) (FV t)) || not (Set.contains var (FV s))) ->
+        Abs(y, substitution s var t)
+    | Abs(y, s) -> Abs(newVar s t, substitution (substitution s (Var(y)) (Var(newVar s t))) var t)
+
+let betaReduction lambda t =
+    match lambda with
+    | Abs(x, s) -> substitution s (Var(x)) t
+    | _ -> lambda
+    
