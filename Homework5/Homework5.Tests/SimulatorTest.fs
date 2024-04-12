da80ed18305899417cbcb616090522a527e37122
Valentin Mirontsev <valentin.mironcev@mail.ru>
2024-04-12 13:00:07 +0300

    add all tests

--- Homework5/Homework5.Tests/SimulatorTest.fs
+++ Homework5/Homework5.Tests/SimulatorTest.fs
@@ -9,8 +9,8 @@ let Setup () =
     ()
 
 [<Test>]
-let Test1 () =
-    let infectedComputer = Computer(Windows)
+let ``Test simulation for bamboo graph`` () =
+    let infectedComputer = Computer Windows
     infectedComputer.Infected <- true;
     let network =
         let computers = [
@@ -25,3 +25,57 @@ let Test1 () =
         Network(computers, connections, virus)
     let infectedComputers = network.Simulate()
     List.length infectedComputers |> should equal 5
+    
+[<Test>]
+let ``Test simulation for sheaf graph`` () =
+    let infectedComputer = Computer Windows
+    infectedComputer.Infected <- true;
+    let network =
+        let computers = [
+            infectedComputer
+            Computer Linux
+            Computer MacOS
+            Computer Windows
+            Computer Linux
+        ]
+        let connections = [[1; 2; 3; 4]; [0]; [0]; [0]; [0]]
+        let virus = Virus(0.8, 0.2, 0.3)
+        Network(computers, connections, virus)
+    let infectedComputers = network.Simulate()
+    List.length infectedComputers |> should equal 5
+    
+[<Test>]
+let ``Test simulation for graph with isolated vertex`` () =
+    let infectedComputer = Computer Windows
+    infectedComputer.Infected <- true;
+    let network =
+        let computers = [
+            infectedComputer
+            Computer Linux
+            Computer MacOS
+            Computer Windows
+            Computer Linux
+        ]
+        let connections = [[1]; [0; 2]; [1; 3]; [2]]
+        let virus = Virus(0.8, 0.2, 0.3)
+        Network(computers, connections, virus)
+    let infectedComputers = network.Simulate()
+    List.length infectedComputers |> should equal 4
+    
+[<Test>]
+let ``Test simulation for graph with computer with 0 infect risk`` () =
+    let infectedComputer = Computer Windows
+    infectedComputer.Infected <- true;
+    let network =
+        let computers = [
+            infectedComputer
+            Computer Linux
+            Computer MacOS
+            Computer Windows
+            Computer MacOS
+        ]
+        let connections = [[1; 2; 3; 4]; [0]; [0]; [0]; [0]]
+        let virus = Virus(0.8, 0, 0.3)
+        Network(computers, connections, virus)
+    let infectedComputers = network.Simulate()
+    List.length infectedComputers |> should equal 4
--- Homework5/Homework5/Program.fs
+++ Homework5/Homework5/Program.fs
@@ -1,2 +1,21 @@
 ﻿module Program
 
+open Simulator
+
+let infectedComputer = Computer(Windows)
+infectedComputer.Infected <- true;
+
+let network =
+    let computers = [
+        infectedComputer
+        Computer Linux
+        Computer MacOS
+        Computer Windows
+        Computer Linux
+    ]
+    let connections = [[1; 2; 3; 4]; [0]; [0]; [0]; [0]]
+    let virus = Virus(0.8, 0.2, 0.3)
+    Network(computers, connections, virus)
+
+let infectedComputers = network.Simulate()
+
