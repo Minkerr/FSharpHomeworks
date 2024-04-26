2c24c30cee9b583406056381546ea4ad1ea33d63
Valentin Mirontsev <valentin.mironcev@mail.ru>
2024-04-26 16:58:28 +0300

    small refactor

--- Controlwork1/Controlwork1/Program.fs
+++ Controlwork1/Controlwork1/Program.fs
@@ -13,16 +13,25 @@ let sumEvenFibonacciLessThanMillion () =
         else sumEvenFibonacciRecursive (sum + numberToAdd) (acc1 + acc2) acc1 (i + 1)
     sumEvenFibonacciRecursive 0 1 0 0
 
+/// <summary>
+/// Class that implements the priority queue data structure
+/// </summary>
 type PriorityQueue() =
     let mutable queue = []
-    member this.Enqueue(item, priority) =
+    /// <summary>
+    /// Add the element to the queue with priority
+    /// </summary>
+    member this.Enqueue(value, priority) =
         let rec enqueueRec items newItem =
             match items with
             | [] -> [newItem]
-            | (_, localPriority) :: _ when priority <= localPriority -> (item, priority) :: items
+            | (_, localPriority) :: _ when priority <= localPriority -> (value, priority) :: items
             | head :: tail -> head :: (enqueueRec tail newItem)
-        queue <- enqueueRec queue (item, priority)
+        queue <- enqueueRec queue (value, priority)
 
+    /// <summary>
+    /// Remove element with lowest priority
+    /// </summary>
     member this.Dequeue() =
         match queue with
         | [] -> failwith "Queue is empty"
@@ -30,8 +39,10 @@ type PriorityQueue() =
             queue <- tail
             fst head
 
+    /// <summary>
+    /// Get element with lowest priority
+    /// </summary>
     member this.Peek() =
         match queue with
         | [] -> failwith "Queue is empty"
         | head :: _ -> fst head
-
