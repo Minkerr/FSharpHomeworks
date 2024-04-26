bc423dafa4fabdfbcf17d86e35872735322d7ec1
Valentin Mirontsev <valentin.mironcev@mail.ru>
2024-04-26 15:43:17 +0300

    add square

---
+++ Controlwork1/Controlwork1.Tests/ControlworkTest.fs
@@ -0,0 +1,21 @@
+module Controlwork1.Tests
+
+open NUnit.Framework
+open Controlwork
+open FsUnit
+
+[<Test>]
+let ``Test square with n = 1`` () =
+    (square 1) |> should equal "*"
+    
+[<Test>]
+let ``Test square with n = 2`` () =
+    (square 2) |> should equal "**\n**"
+    
+[<Test>]
+let ``Test square with n = 4`` () =
+    (square 4) |> should equal "****\n*  *\n*  *\n****"
+    
+[<Test>]
+let ``Test square with n = 0`` () =
+    (square 0) |> should equal ""
---
+++ Controlwork1/Controlwork1/Program.fs
@@ -0,0 +1,8 @@
+﻿module Controlwork
+
+let square n =
+    let constructLine x =
+        if (x = n || x = 1) then (String.replicate n "*")
+            else ("*" + (String.replicate (n - 2) " ") + "*")
+    String.concat "\n" (List.map constructLine [1..n])
+  
