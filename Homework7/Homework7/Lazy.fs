6fe6243bd72478ec0e48e4b1a4d3d97965a51bbb
Valentin Mirontsev <valentin.mironcev@mail.ru>
2024-05-03 23:59:09 +0300

    add concurrent lazy

--- Homework7/Homework7/Lazy.fs
+++ Homework7/Homework7/Lazy.fs
@@ -1,5 +1,7 @@
 module Homework7.Lazy
 
+open System.Threading
+
 type ILazy<'a> =
     abstract member Get: unit -> 'a
 
@@ -14,3 +16,27 @@ type Lazy<'a>(supplier : unit -> 'a) =
                 value <- Some(temp)
                 temp
                 
+type ConcurrentLazy<'a>(supplier : unit -> 'a) =
+    let locker = obj()
+    let mutable value = None
+    interface ILazy<'a> with
+        member this.Get() : 'a =
+            match value with
+            | Some x -> x
+            | None ->
+                lock locker ( fun () ->
+                    let temp = supplier ()
+                    value <- Some(temp)
+                    temp) 
+                
+type LockFreeLazy<'a>(supplier : unit -> 'a) =
+    let locker = obj()
+    let mutable value = None
+    interface ILazy<'a> with
+        member this.Get() : 'a =
+            match value with
+            | Some x -> x
+            | None ->
+                let temp = supplier ()
+                Volatile.Write(ref value, Some(temp)) 
+                temp
