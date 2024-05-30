506e7e387409ff5045d20d488276caaf71d6278b
Valentin Mirontsev <valentin.mironcev@mail.ru>
2024-05-30 12:16:49 +0300

    add rounding

---
+++ Homework6/Homework6.Tests/RoundingTest.fs
@@ -0,0 +1,26 @@
+module Homework6.Tests
+
+open NUnit.Framework
+open FsUnit
+
+[<Test>]
+let ``Rounding 3 with accuracy equals should work for simple test`` () =
+    let rounding = RoundingBuilder(3)
+    let act =
+        rounding {
+            let! a = 2.0 / 12.0
+            let! b = 3.5
+            return a / b
+        }
+    act - 0.001 |> should lessThan 0.048
+    act + 0.001 |> should greaterThan 0.048
+    
+[<Test>]
+let ``Rounding with 0 accuracy should make result approximately integer`` () =
+    let rounding = RoundingBuilder(0)
+    let act =
+        rounding {
+            return 5.0 / 3.0
+        }
+    act - 0.001 |> should lessThan 2.0
+    act + 0.001 |> should greaterThan 2.0
---
+++ Homework6/Homework6/Program.fs
@@ -0,0 +1,9 @@
+﻿module Homework6
+
+type RoundingBuilder(accuracy : int) =
+    member this.Bind(x, f) =
+        f x
+    member this.Return(x : float) =
+        System.Math.Round(x, accuracy) 
+    
+
