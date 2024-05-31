c819363b9961283fbce1fd68671511e4205ab822
Valentin Mirontsev <valentin.mironcev@mail.ru>
2024-06-01 01:05:59 +0300

    rebuild tree

--- Homework2/Homework2.Tests/PrepositionCalculateTest.fs
+++ Homework2/Homework2.Tests/PrepositionCalculateTest.fs
@@ -10,9 +10,12 @@ let ``The value of Number(1) preposition tree is 1`` () =
     
 [<Test>]
 let ``The value of Add(Number(2), Number(2)) preposition tree is 4`` () =
-    Add(Number(2), Number(2)) |> calculate |> should equal 4
+    BinaryOperation(Number(2), "+", Number(2)) |> calculate |> should equal 4
     
 [<Test>]
 let ``The value of big preposition tree is `` () =
-    let act = Multiply(Subtract(Number(12), Number(1)), Divide(Number(15), Number(5))) |> calculate
+    let act = BinaryOperation(
+        BinaryOperation(Number(12),"-", Number(1)), "*",
+        BinaryOperation(Number(15), "/", Number(5)))
+              |> calculate
     act |> should equal 33
--- Homework2/Homework2/PrepositionCalculate.fs
+++ Homework2/Homework2/PrepositionCalculate.fs
@@ -2,16 +2,16 @@ module PrepositionCalculate
 
 type Proposition =
     | Number of int
-    | Add of Proposition * Proposition
-    | Subtract of Proposition * Proposition
-    | Multiply of Proposition * Proposition
-    | Divide of Proposition * Proposition
+    | BinaryOperation of Proposition * string * Proposition
     
 let rec calculate prep =
     match prep with
     | Number(x) -> x
-    | Add(x1, x2) -> calculate x1 + calculate x2
-    | Subtract(x1, x2) -> calculate x1 - calculate x2
-    | Multiply(x1, x2) -> calculate x1 * calculate x2
-    | Divide(x1, x2) -> calculate x1 / calculate x2
+    | BinaryOperation(l, o, r) ->
+        match o with
+        | "+" -> calculate l + calculate r
+        | "-" -> calculate l - calculate r
+        | "*" -> calculate l * calculate r
+        | "/" -> calculate l / calculate r
+        | _ -> failwith "Incorrect operation"
 
