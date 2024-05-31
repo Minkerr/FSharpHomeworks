bc0f5f2d3953a0f8cce4f1ec3587867c36594ff3
Valentin Mirontsev <valentin.mironcev@mail.ru>
2024-06-01 02:25:15 +0300

    add brackets

--- Homework4/Homework4/CorrectBracketSequence.fs
+++ Homework4/Homework4/CorrectBracketSequence.fs
@@ -1,7 +1,9 @@
 module Homework4.CorrectBracketSequence
 
 let checkStringForCorrectBracketSequence input =
-    let getOpenBracket c =
+    let closingBrackets = [')'; '}'; ']']
+    let openingBrackets = ['('; '{'; '[']
+    let getOpeningBracket c =
         match c with
         |')' -> '('
         |'}' -> '{'
@@ -9,8 +11,11 @@ let checkStringForCorrectBracketSequence input =
     let rec CBSRec (input : string) i (stack : char List) =
         if i = input.Length then stack.IsEmpty else 
             match input[i] with
-            | c when (c = '(' || c = '{' || c = '[') -> CBSRec input (i + 1) (c :: stack)
-            | c when (c = ')' || c = '}' || c = ']') -> 
-                if stack.IsEmpty || not (stack.Head = getOpenBracket c) then false else CBSRec input (i + 1) stack.Tail
+            | c when (List.contains c openingBrackets) -> CBSRec input (i + 1) (c :: stack)
+            | c when (List.contains c closingBrackets) ->
+                match stack with
+                | [] -> false
+                | head :: _ when head <> getOpeningBracket c -> false
+                | _ -> CBSRec input (i + 1) stack.Tail
             | _ -> CBSRec input (i + 1) stack
     CBSRec input 0 []
